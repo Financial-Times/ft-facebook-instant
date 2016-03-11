e95422e75c3f7a630e82c3799edd8ceca76e2f88
George Crawford <george.crawford@ft.com>
2016-03-11 20:14:07 +0000

    HTTP Basic Auth in production

--- main.js
+++ main.js
@@ -2,11 +2,21 @@
 const express = require('express');
 const ftwebservice = require('express-ftwebservice');
 const authS3O = require('s3o-middleware');
+const assertEnv = require('@quarterto/assert-env');
 
 const port = process.env.PORT || 6247;
 const app = express();
 
 ftwebservice(app, require('./server/controllers/ftwebservice.js'));
+
+if(app.get('env') === 'production') {
+	assertEnv([
+		'HTTP_AUTH_PASS',
+	]);
+} else {
+	assertEnv([
+	]);
+}
 
 // Routes which don't require Staff Single Sign-On
 app.get('/feed', require('./server/controllers/feed.js'));
--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,7 +1,26 @@
 "use strict";
 
 const feed = require('../lib/feed');
+const auth = require('basic-auth');
+
+const checkAuth = req => {
+
+	if ('production' !== process.env.NODE_ENV) return true;
+
+	const credentials = auth(req);
+	if (credentials && credentials.name === 'facebook' && credentials.pass === process.env.HTTP_AUTH_PASS) {
+		return true;
+	}
+
+	return false;
+};
 
 module.exports = (req, res) => {
+	if (!checkAuth(req)) {
+		res.statusCode = 401;
+		res.setHeader('WWW-Authenticate', 'Basic realm="feed"');
+		return res.end('Access denied');
+	}
+
 	res.send(feed.generate());
 };
