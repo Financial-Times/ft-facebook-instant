a7a80b9e993af9771eb4e57dc3820bf8b7d8045f
George Crawford <george.crawford@ft.com>
2016-03-11 17:59:22 +0000

    S30

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 'use strict';
 const express = require('express');
 const ftwebservice = require('express-ftwebservice');
+const authS3O = require('s3o-middleware');
+
 const path = require('path');
 
 const port = process.env.PORT || 6247;
@@ -32,5 +34,12 @@ ftwebservice(app, {
 		],
 	},
 });
+
+// Routes which don't require Staff Single Sign-On
+app.get('/feed', require('./server/controllers/feed.js'));
+
+// Routes which require Staff Single Sign-On
+app.use(authS3O);
+app.get('/admin', require('./server/controllers/admin.js'));
 
 app.listen(port, () => console.log('Up and running on port', port));
