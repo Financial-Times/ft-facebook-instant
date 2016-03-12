c2eca9acc22bc3e15bb15be6f6fdefbc7c681b2e
George Crawford <george.crawford@ft.com>
2016-03-12 13:51:07 +0000

    Debugging

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -4,9 +4,11 @@ const feed = require('../lib/feed');
 const auth = require('basic-auth');
 
 const checkAuth = req => {
+	console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 	if(process.env.NODE_ENV !== 'production') return true;
 
 	const credentials = auth(req);
+	console.log('credentials', credentials);
 	if(credentials && credentials.name === 'facebook' && credentials.pass === process.env.HTTP_AUTH_PASS) {
 		return true;
 	}
@@ -20,6 +22,8 @@ module.exports = (req, res) => {
 		res.setHeader('WWW-Authenticate', 'Basic realm="feed"');
 		return res.end('Access denied');
 	}
+
+	console.log('UA', req.get('User-Agent'));
 
 	res.set('Content-Type', 'application/rss+xml');
 	res.send(feed.generate());
