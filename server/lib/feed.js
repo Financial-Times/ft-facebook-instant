c14c85585f2eaa85d01f63bc8d7b5f0fb17a1065
George Crawford <george.crawford@ft.com>
2016-03-21 11:10:23 +0000

    Add test article to feed

--- server/lib/feed.js
+++ server/lib/feed.js
@@ -10,30 +10,29 @@ module.exports.generate = type => {
 		generator: 'https://github.com/Financial-Times/ft-facebook-instant',
 	});
 
-	const fixturePath = require('path').resolve(__dirname, '../../test/fixtures/facebook-test-article.html');
+	const fixturePath = require('path').resolve(__dirname, '../../test/fixtures/94e97eee-ce9a-11e5-831d-09f7778e7377.html');
 	const article = require('fs').readFileSync(fixturePath, 'utf8');
 
-	// See: https://developers.facebook.com/docs/instant-articles/publishing
 	feed.item({
 		// The headline of the article.
-		title: 'The headline of the article.',
+		title: 'Why millennials go on holiday instead of saving for a pension',
 
 		// A string that provides a unique identifier for this article in your feed.
-		guid: 'A string that provides a unique identifier for this article in your feed.',
+		guid: '94e97eee-ce9a-11e5-831d-09f7778e7377',
 
 		custom_elements: [
 			// A summary of your article, in plain text form.
-			{description: 'A summary of your article, in plain text form.'},
+			{description: '20-somethings feel financially doomed so do not save'},
 
 			// 	Name of the person who wrote the article. Use multiple <author> elements for
 			// 	multiple authors.
-			{author: 'George Crawford'},
+			{author: 'Aime Williams'},
 
 			// The canonical URL for this article on your site.
-			{link: 'http://www.ft.com/content/e8813dd4-d00d-11e5-831d-09f7778e7377'},
+			{link: 'http://www.ft.com/content/94e97eee-ce9a-11e5-831d-09f7778e7377'},
 
 			// The date of the article’s publication, in ISO-8601 format.
-			{pubDate: '1994-11-05T13:15:30Z'},
+			{pubDate: '2016-02-12T10:34:09Z'},
 
 			// The full content of your article, in HTML form. Remember to escape all HTML
 			// content by wrapping it within a CDATA section.
