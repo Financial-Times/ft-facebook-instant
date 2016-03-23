6262dfdf20f0cef14bc99198d56b17ea4dd64286
George Crawford <george.crawford@ft.com>
2016-03-23 21:11:07 +0000

    Test UUIDs

--- server/lib/testUuids.js
+++ server/lib/testUuids.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = [
-	{uuid: '38587178-9824-11e5-95c7-d47aa298f769', description: 'George: TODO'},
+	{uuid: '3ee29f96-88e0-11e3-bb5f-00144feab7de', description: 'Editorial test article'},
+	{uuid: '70d0bfd8-d1b3-11e5-831d-09f7778e7377', description: 'What are the economic consequences of Brexit?'},
+	{uuid: 'cf290804-25e9-3e4b-8a19-efc090b6fca0', description: 'Blogs: #techFT – From phones to drones at MWC'},
+	{uuid: '9bc4c15c-cf57-11e5-92a1-c5e23ef99c77', description: 'life and arts: Hieronymus Bosch: one hell of a hometown exhibition'},
+	{uuid: '9275f196-dfdd-3361-a375-f50e8b2d9a03', description: 'brussels briefing: Brexit day'},
+	{uuid: '0efb495a-d446-11e5-829b-8564e7528e54', description: 'firstFT: Saudi-Russia oil deal, digital divide and a policy that dare not speak its name'},
+	{uuid: 'c0e910c4-d005-11e5-831d-09f7778e7377', description: 'Lunch with the FT: Alan Yentob'},
+	{uuid: 'd5ac853a-978a-11e5-95c7-d47aa298f769', description: 'Slideshow: Boost for China as it joins IMF elite'},
 ];
