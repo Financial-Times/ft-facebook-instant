9801143fe13746ac0822150daf10094149f9b360
George Crawford <george.crawford@ft.com>
2016-03-23 21:24:23 +0000

    Ft.com links

--- server/lib/testUuids.js
+++ server/lib/testUuids.js
@@ -6,7 +6,7 @@ module.exports = [
 	{uuid: 'cf290804-25e9-3e4b-8a19-efc090b6fca0', description: 'Blogs: #techFT – From phones to drones at MWC'},
 	{uuid: '9bc4c15c-cf57-11e5-92a1-c5e23ef99c77', description: 'life and arts: Hieronymus Bosch: one hell of a hometown exhibition'},
 	{uuid: '9275f196-dfdd-3361-a375-f50e8b2d9a03', description: 'brussels briefing: Brexit day'},
-	{uuid: '0efb495a-d446-11e5-829b-8564e7528e54', description: 'firstFT: Saudi-Russia oil deal, digital divide and a policy that dare not speak its name'},
+	{uuid: '0efb495a-d446-11e5-829b-8564e7528e54', description: 'firstFT: Saudi-Russia oil deal...'},
 	{uuid: 'c0e910c4-d005-11e5-831d-09f7778e7377', description: 'Lunch with the FT: Alan Yentob'},
 	{uuid: 'd5ac853a-978a-11e5-95c7-d47aa298f769', description: 'Slideshow: Boost for China as it joins IMF elite'},
 ];
