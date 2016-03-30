2e8385bfef8665abb42c7f19b66598a72f2e1118
George Crawford <george.crawford@ft.com>
2016-03-30 15:15:27 +0100

    Linting

--- server/lib/xslt.js
+++ server/lib/xslt.js
@@ -2,48 +2,51 @@
 
 const spawn = require('child_process').spawn;
 
-module.exports = (xml, stylesheet, params) => new Promise(function(resolve, reject) {
-	var output = [];
-	var errors = [];
-	var options = [
+module.exports = (xml, stylesheet, params) => new Promise((resolve, reject) => {
+	const output = [];
+	const errors = [];
+
+	let options = [
 		'--html',
 		'--novalid',
-		'--encoding', 'utf-8'
+		'--encoding', 'utf-8',
 	];
 
-	params && Object.keys(params).forEach(function(param) {
-		var string = typeof params[param] === 'string';
-		options = options.concat(string ? '--stringparam' : '--param', param, params[param]);
-	});
+	if(Array.isArray(params)) {
+		Object.keys(params).forEach(param => {
+			const string = typeof params[param] === 'string';
+			options = options.concat(string ? '--stringparam' : '--param', param, params[param]);
+		});
+	}
 
-	var env = {PATH: `${process.env.PATH}:${process.cwd()}/libxslt/bin`};
-	var xsltproc = spawn('xsltproc', options.concat(
+	const env = {PATH: `${process.env.PATH}:${process.cwd()}/libxslt/bin`};
+	const xsltproc = spawn('xsltproc', options.concat(
 		stylesheet,
 		'-'
 	), {env});
 
-	xsltproc.stdin.on('error', function(error) {
+	xsltproc.stdin.on('error', error => {
 		errors.push(error.toString());
-		return reject('xsltproc stdin error: ' + errors);
+		return reject(`xsltproc stdin error: ${errors}`);
 	});
 
-	xsltproc.on('error', function(error) {
+	xsltproc.on('error', error => {
 		console.log(error);
 		errors.push(error.toString());
-		return reject('xsltproc error: ' + errors);
+		return reject(`xsltproc error: ${errors}`);
 	});
 
-	xsltproc.stdout.on('data', function(data) {
+	xsltproc.stdout.on('data', data => {
 		output.push(data);
 	});
 
-	xsltproc.stderr.on('data', function(error) {
+	xsltproc.stderr.on('data', error => {
 		errors.push(error.toString());
 	});
 
-	xsltproc.on('close', function(code) {
+	xsltproc.on('close', code => {
 		if(code !== 0) {
-			return reject('xsltproc exited with code ' + code + ': ' + errors);
+			return reject(`xsltproc exited with code ${code}: ${errors}`);
 		}
 
 		resolve(output.join('').replace(/<\/?html>/g, ''));
