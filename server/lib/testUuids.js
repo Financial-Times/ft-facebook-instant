b17f7cb7410289aa0853d436bf61086764bef269
George Crawford <george.crawford@ft.com>
2016-04-02 20:34:15 +0100

    More informative

--- server/lib/testUuids.js
+++ server/lib/testUuids.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = [
-	{uuid: '3ee29f96-88e0-11e3-bb5f-00144feab7de', description: 'Editorial test article'},
+	{uuid: '3ee29f96-88e0-11e3-bb5f-00144feab7de', description: 'Lead headline (Editorial test article)'},
 	{uuid: '94e97eee-ce9a-11e5-831d-09f7778e7377', description: 'Why millennials go on holiday instead of saving for a pension'},
 	{uuid: '70d0bfd8-d1b3-11e5-831d-09f7778e7377', description: 'What are the economic consequences of Brexit?'},
 	{uuid: 'cf290804-25e9-3e4b-8a19-efc090b6fca0', description: 'Blogs: #techFT – From phones to drones at MWC'},
@@ -9,7 +9,7 @@ module.exports = [
 	{uuid: '9275f196-dfdd-3361-a375-f50e8b2d9a03', description: 'brussels briefing: Brexit day'},
 	{uuid: '0efb495a-d446-11e5-829b-8564e7528e54', description: 'firstFT: Saudi-Russia oil deal...'},
 	{uuid: 'c0e910c4-d005-11e5-831d-09f7778e7377', description: 'Lunch with the FT: Alan Yentob'},
-	{uuid: 'd5ac853a-978a-11e5-95c7-d47aa298f769', description: 'Slideshow: Boost for China as it joins IMF elite'},
-	{uuid: '6348fc2c-5778-3778-983a-d54d948da828', description: 'Tweet in blockquote'},
-	{uuid: '2f1774f4-ab65-11e3-aad9-00144feab7de', description: 'Promo box and pull quote'},
+	{uuid: 'd5ac853a-978a-11e5-95c7-d47aa298f769', description: 'Boost for China as it joins IMF elite (Slideshow)'},
+	{uuid: '6348fc2c-5778-3778-983a-d54d948da828', description: 'This is nuts. When’s the crash? (Tweet in blockquote)'},
+	{uuid: '2f1774f4-ab65-11e3-aad9-00144feab7de', description: 'Seeing the wood for the trees in pensions (Promo box and pull quote)'},
 ];
