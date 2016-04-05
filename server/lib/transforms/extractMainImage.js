2639b5e177f6436fff4c8a129ea22988f01c1359
George Crawford <george.crawford@ft.com>
2016-04-05 11:50:54 +0100

    No aspect-ratio stuf yet

--- server/lib/transforms/extractMainImage.js
+++ server/lib/transforms/extractMainImage.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const targetAspectRatio = 4 / 3; // See https://developers.facebook.com/docs/instant-articles/reference/cover
-
 module.exports = $ => {
 	// find image in the body
 	const $figure = $('figure').eq(0);
@@ -12,21 +10,10 @@ module.exports = $ => {
 			!$figure.prev().length &&
 			(!$figure.parent() || !$figure.parent().prev().length)
 	) {
+		const $figcaption = $('figcaption', $figure);
+		if($figcaption) $figcaption.addClass('op-large');
+
 		$figure.remove();
-
-		const $img = $('img', $figure);
-
-		const width = parseInt($img.attr('width'), 10);
-		const height = parseInt($img.attr('height'), 10);
-		const ratio = width / height;
-
-		let targetWidth, targetHeight;
-		// if(ratio > targetAspectRatio) {
-		// 	targetWidth =
-		// }
-
-		console.log({width, height, ratio});
-
 		return $.html($figure);
 	}
 };
