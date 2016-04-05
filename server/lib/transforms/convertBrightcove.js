fb59a875b6617ff3fc4db8ac1507a00752dd432e
George Crawford <george.crawford@ft.com>
2016-04-05 16:11:23 +0100

    Linting, easier UI

--- server/lib/transforms/convertBrightcove.js
+++ server/lib/transforms/convertBrightcove.js
@@ -10,22 +10,24 @@ const encodeId = id => {
 	}
 };
 
+const replaceHtml = ($, el) => {
+	const $brightcove = $(el);
+
+	const width = 480;
+	const height = 270;
+	const account = $brightcove.attr('data-account');
+	const playerid = $brightcove.attr('data-player');
+	const videoId = $brightcove.attr('data-video-id');
+	const embed = 'default';
+
+	const src = `https://players.brightcove.net/${encodeURIComponent(account)}/`
+		+ `${encodeURIComponent(playerid)}_${encodeURIComponent(embed)}/index.html?videoId=${encodeId(videoId)}`;
+
+	$brightcove.replaceWith($(`<figure class="op-interactive"><iframe width="${width}" height="${height}" src="${src}"></iframe></figure>`));
+};
+
 module.exports = $ => Promise.resolve()
 .then(() => {
-	$('brightcove').map((index, el) => {
-		const $brightcove = $(el);
-
-		const width = 480;
-		const height = 270;
-		const account = $brightcove.attr('data-account');
-		const playerid = $brightcove.attr('data-player');
-		const videoId = $brightcove.attr('data-video-id');
-		const embed = 'default';
-
-		const src = `https://players.brightcove.net/${encodeURIComponent(account)}/`
-			+ `${encodeURIComponent(playerid)}_${encodeURIComponent(embed)}/index.html?videoId=${encodeId(videoId)}`;
-
-		$brightcove.replaceWith($(`<figure class="op-interactive"><iframe width="${width}" height="${height}" src="${src}"></iframe></figure>`));
-	});
+	$('brightcove').map((index, el) => replaceHtml($, el));
 })
 .then(() => $);
