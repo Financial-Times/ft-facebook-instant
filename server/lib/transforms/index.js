a6465b3bc547c7f6b2de5bc75c99581d70a7063c
George Crawford <george.crawford@ft.com>
2016-04-06 15:50:35 +0100

    Remove empty elements

--- server/lib/transforms/index.js
+++ server/lib/transforms/index.js
@@ -5,6 +5,7 @@ const cheerio = require('cheerio');
 const stripBody = require('./stripBody');
 const addImageDimensions = require('./addImageDimensions');
 const convertBrightcove = require('./convertBrightcove');
+const removeEmptyElements = require('./removeEmptyElements');
 
 module.exports = body => {
 	const $ = cheerio.load(body, {decodeEntities: false});
@@ -13,6 +14,7 @@ module.exports = body => {
 		stripBody,
 		addImageDimensions,
 		convertBrightcove,
+		removeEmptyElements,
 	].map(transform => transform($)))
 	.then(() => $);
 };
