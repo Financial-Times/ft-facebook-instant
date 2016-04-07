fae9a49c6d7138716bbe6e9550c2a01313bd1fc0
George Crawford <george.crawford@ft.com>
2016-04-07 18:19:47 +0100

    UUID which has captions on each image

--- server/lib/testUuids.js
+++ server/lib/testUuids.js
@@ -5,7 +5,7 @@ module.exports = [
 	{uuid: '94e97eee-ce9a-11e5-831d-09f7778e7377', description: 'Why millennials go on holiday instead of saving for a pension'},
 	{uuid: 'cf290804-25e9-3e4b-8a19-efc090b6fca0', description: 'Blogs: #techFT – From phones to drones at MWC (Blog)'},
 	{uuid: '0efb495a-d446-11e5-829b-8564e7528e54', description: 'firstFT: Saudi-Russia oil deal... (FirstFT)'},
-	{uuid: 'd5ac853a-978a-11e5-95c7-d47aa298f769', description: 'Boost for China as it joins IMF elite (Slideshow)'},
+	{uuid: '43e4aea0-2b2b-11e5-acfb-cbd2e1c81cca', description: 'Virtual reality: The next frontier (Slideshow)'},
 	{uuid: '6348fc2c-5778-3778-983a-d54d948da828', description: 'This is nuts. When’s the crash? (Tweet in blockquote)'},
 	{uuid: '2f1774f4-ab65-11e3-aad9-00144feab7de', description: 'Seeing the wood for the trees in pensions (Promo box and pull quote)'},
 	{uuid: 'adc52e62-f1e6-11e5-9f20-c3a047354386', description: 'Schroders dumps PwC as its auditor after 57 years (Big number)'},
