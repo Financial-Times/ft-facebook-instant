d9f1ca56f34fa8dc99a60461a89ee7d91c98afd7
George Crawford <george.crawford@ft.com>
2016-04-08 14:27:14 +0100

    Sorting, and tweak UI

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -9,8 +9,8 @@ module.exports = (req, res, next) => fbApi.list({fields: ['canonical_url']})
 	const promises = fbList.map(({canonical_url}) => articleModel.ensureInDb(canonical_url));
 	return Promise.all(promises);
 })
+.then(fbList => fbList.sort((a, b) => b.date_record_updated - a.date_record_updated))
 .then(fbList => Promise.all(fbList.map(fbItem => articleModel.get(fbItem.canonical))))
-// .then(articles => res.json(articles));
 .then(articles => res.render('index', {
 	articles,
 	testUuids,
