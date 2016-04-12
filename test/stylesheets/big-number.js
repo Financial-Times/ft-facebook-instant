022996f87f4d7bdee08400e2ae6f42661b1483c2
Matt Brennan <mattyb1000@gmail.com>
2016-04-12 16:26:45 +0100

    chai-as-promised

--- test/stylesheets/big-number.js
+++ test/stylesheets/big-number.js
@@ -6,16 +6,15 @@ const xslt = require('../../build/lib/xslt');
 describe('big number transform', () => {
 	const transform = input => xslt(input, `${process.cwd()}/server/stylesheets/main.xsl`);
 
-	it('should transform some markup', (done) => {
-		transform(`<div class="n-content-big-number">
+	it('should transform some markup', () =>
+		expect(transform(`<div class="n-content-big-number">
 			<span class="n-content-big-number__title"><p>10.48m</p></span>
 			<span class="n-content-big-number__content">
 				<p>Record number of barrels a day that oil refiners raised processing runs to last year</p>
 			</span>
-		</div>`)
-		.then(output => expect(output).dom.to.equal(`<aside>
+		</div>`)).dom.to.eventually.equal(`<aside>
 			<p>10.48m</p>
 			<cite><p>Record number of barrels a day that oil refiners raised processing runs to last year</p></cite>
-		</aside>`)).then(() => done(), done);
-	});
+		</aside>`)
+	);
 });
