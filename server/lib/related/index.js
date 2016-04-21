67ed37fe385043b2341f2155fd7ece14e653936e
Matt Brennan <mattyb1000@gmail.com>
2016-04-21 12:14:35 +0100

    lint

--- server/lib/related/index.js
+++ server/lib/related/index.js
@@ -6,7 +6,7 @@ const fns = [
 
 module.exports = async article => {
 	const articles = new Set();
-	for(let fn of fns) {
+	for(const fn of fns) {
 		(await fn(article)).forEach(moreArticle => articles.add(moreArticle));
 
 		if(articles.size >= 3) break;
