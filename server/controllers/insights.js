9e9a9ba294f2b5608ea69ab92fd8d99e44b399cb
George Crawford <george.crawford@ft.com>
2016-04-25 12:57:24 +0100

    WIP

--- server/controllers/insights.js
+++ server/controllers/insights.js
@@ -1,5 +1,26 @@
 'use strict';
 
-module.exports = (req, res, next) => Promise.resolve()
-.then(() => res.json({blah: 123}))
+const fbApi = require('../lib/fbApi');
+const pageId = process.env.FB_PAGE_ID;
+
+const addPostDetails = posts => {
+	const batch = posts.map(post => ({
+		method: 'get',
+		relative_url: `${post.id}?fields=link,created_time,message,id,description,is_published`,
+	}));
+
+	return fbApi.call('', 'post', {batch})
+		.then(batchResults => batchResults.map((result, index) => JSON.parse(result.body)));
+};
+
+module.exports = (req, res, next) => fbApi.call(
+	`/${pageId}/posts`,
+	'GET',
+	{
+		// TODO: pagination
+		limit: 10,
+	}
+)
+.then(posts => addPostDetails(posts.data))
+.then(posts => res.json(posts))
 .catch(next);
