8fafb38833c10d03b400575225dc6fff6049415c
George Crawford <george.crawford@ft.com>
2016-05-03 15:05:36 +0100

    More reliable NPM stats

--- server/controllers/insights.js
+++ server/controllers/insights.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fbApi = require('../lib/fbApi');
-const stats = require('stats-lite');
+const numbers = require('numbers');
 const denodeify = require('denodeify');
 const csvStringify = denodeify(require('csv-stringify'));
 
@@ -142,14 +142,14 @@ const aggregateMetricBreakdowns = (data) => {
 	return Object.assign(aggregations, {
 		min: values[0],
 		max: values[values.length - 1],
-		mean: stats.mean(values),
-		median: stats.median(values),
-		mode: stats.mode(values),
-		stdev: stats.stdev(values),
-		p25: stats.percentile(values, 0.25),
-		p50: stats.percentile(values, 0.50),
-		p75: stats.percentile(values, 0.75),
-		p95: stats.percentile(values, 0.95),
+		mean: numbers.statistic.mean(values),
+		median: numbers.statistic.median(values),
+		mode: numbers.statistic.mode(values),
+		stdev: numbers.statistic.standardDev(values),
+		p25: numbers.statistic.quantile(values, 25, 100),
+		p50: numbers.statistic.quantile(values, 50, 100),
+		p75: numbers.statistic.quantile(values, 75, 100),
+		p95: numbers.statistic.quantile(values, 95, 100),
 	});
 };
 
@@ -160,7 +160,7 @@ const flattenIaMetrics = (post, flat) => {
 		if(metric) {
 			switch(iaMetricTypes[key]) {
 				case 'day':
-					flat[`ia_${key}`] = metric.data.reduce((total, item) => (total + parseInt(item.value, 0)), 0);
+					flat[`ia_${key}`] = metric.data.reduce((total, item) => (total + parseInt(item.value, 10)), 0);
 					break;
 				case 'week':
 					aggregations = aggregateMetricBreakdowns(metric.data);
@@ -188,9 +188,9 @@ const flattenPost = post => {
 		updated_time: post.updated_time,
 	};
 
-	flat.shares = post.shares.count;
-	flat.likes = post.likes.summary.total_count;
-	flat.comments = post.comments.summary.total_count;
+	flat.shares = post.shares && post.shares.count;
+	flat.likes = post.likes && post.likes.summary.total_count;
+	flat.comments = post.comments && post.comments.summary.total_count;
 
 	Object.keys(post.insights).forEach(insightKey => {
 		const insight = post.insights[insightKey];
@@ -234,8 +234,7 @@ const processResults = ([posts, links, canonicals]) => Object.keys(posts).map(id
 	}
 
 	return post;
-})
-.map(flattenPost);
+});
 
 const executeQuery = ids => fbApi.call('', 'POST', {
 	batch: createQuery(ids),
@@ -258,73 +257,14 @@ const batchIdList = idList => {
 
 const generateCsv = data => csvStringify(data, {
 	header: true,
-	columns: {
-		id: 'id',
-		type: 'type',
-		name: 'name',
-		link: 'link',
-		created_time: 'created_time',
-		message: 'message',
-		description: 'description',
-		is_published: 'is_published',
-		updated_time: 'updated_time',
-		shares: 'shares',
-		likes: 'likes',
-		comments: 'comments',
-		insight_post_impressions: 'insight_post_impressions',
-		insight_post_impressions_unique: 'insight_post_impressions_unique',
-		insight_post_impressions_fan_unique: 'insight_post_impressions_fan_unique',
-		insight_post_impressions_organic_unique: 'insight_post_impressions_organic_unique',
-		insight_post_impressions_viral: 'insight_post_impressions_viral',
-		insight_post_impressions_viral_unique: 'insight_post_impressions_viral_unique',
-		insight_post_impressions_by_story_type_other: 'insight_post_impressions_by_story_type_other',
-		insight_post_impressions_by_story_type_unique_other: 'insight_post_impressions_by_story_type_unique_other',
-		insight_post_consumptions: 'insight_post_consumptions',
-		insight_post_consumptions_unique: 'insight_post_consumptions_unique',
-		insight_post_consumptions_by_type_other_clicks: 'insight_post_consumptions_by_type_other_clicks',
-		insight_post_consumptions_by_type_link_clicks: 'insight_post_consumptions_by_type_link_clicks',
-		insight_post_consumptions_by_type_unique_other_clicks: 'insight_post_consumptions_by_type_unique_other_clicks',
-		insight_post_consumptions_by_type_unique_link_clicks: 'insight_post_consumptions_by_type_unique_link_clicks',
-		insight_post_engaged_users: 'insight_post_engaged_users',
-		insight_post_negative_feedback: 'insight_post_negative_feedback',
-		insight_post_engaged_fan: 'insight_post_engaged_fan',
-		insight_post_fan_reach: 'insight_post_fan_reach',
-		insight_post_stories_by_action_type_share: 'insight_post_stories_by_action_type_share',
-		insight_post_stories_by_action_type_like: 'insight_post_stories_by_action_type_like',
-		insight_post_stories_by_action_type_comment: 'insight_post_stories_by_action_type_comment',
-		canonical: 'canonical',
-		canonical_share: 'canonical_share',
-		canonical_comment: 'canonical_comment',
-		ia_published: 'ia_published',
-		ia_import_status: 'ia_import_status',
-		ia_all_views: 'ia_all_views',
-		ia_all_view_durations_min: 'ia_all_view_durations_min',
-		ia_all_view_durations_max: 'ia_all_view_durations_max',
-		ia_all_view_durations_mean: 'ia_all_view_durations_mean',
-		ia_all_view_durations_median: 'ia_all_view_durations_median',
-		ia_all_view_durations_mode: 'ia_all_view_durations_mode',
-		ia_all_view_durations_stdev: 'ia_all_view_durations_stdev',
-		ia_all_view_durations_p25: 'ia_all_view_durations_p25',
-		ia_all_view_durations_p50: 'ia_all_view_durations_p50',
-		ia_all_view_durations_p75: 'ia_all_view_durations_p75',
-		ia_all_view_durations_p95: 'ia_all_view_durations_p95',
-		ia_all_scrolls_min: 'ia_all_scrolls_min',
-		ia_all_scrolls_max: 'ia_all_scrolls_max',
-		ia_all_scrolls_mean: 'ia_all_scrolls_mean',
-		ia_all_scrolls_median: 'ia_all_scrolls_median',
-		ia_all_scrolls_mode: 'ia_all_scrolls_mode',
-		ia_all_scrolls_stdev: 'ia_all_scrolls_stdev',
-		ia_all_scrolls_p25: 'ia_all_scrolls_p25',
-		ia_all_scrolls_p50: 'ia_all_scrolls_p50',
-		ia_all_scrolls_p75: 'ia_all_scrolls_p75',
-		ia_all_scrolls_p95: 'ia_all_scrolls_p95',
-	},
 });
 
 module.exports = (req, res, next) => getPostsLists({since: '2016-04-26'})
 .then(result => (batchIdList(result.data)))
 .then(idBatch => Promise.all(idBatch.map(executeQuery)))
 .then(batchedResults => [].concat(...batchedResults))
+
+.then(posts => posts.map(flattenPost))
 .then(generateCsv)
 .then(csv => {
 	res.header('Content-Type', 'text/csv');
