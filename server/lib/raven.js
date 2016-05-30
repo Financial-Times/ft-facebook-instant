4d4aff7e9c740e98302cf3efcf10229e448e7847
George Crawford <george.crawford@ft.com>
2016-05-30 12:07:39 +0100

    revert experiments

--- server/lib/raven.js
+++ server/lib/raven.js
@@ -6,9 +6,9 @@ const os = require('os');
 const ravenClient = new raven.Client(process.env.SENTRY_DSN, {
 	release: process.env.HEROKU_SLUG_COMMIT,
 });
-ravenClient.setExtraContext({
+ravenClient.setExtraContext({env: process.env});
+ravenClient.setTagsContext({
 	server_name: process.env.HEROKU_APP_NAME || os.hostname(),
-	env: process.env,
 });
 
 module.exports = ravenClient;
