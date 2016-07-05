a4bbe6e2baf05272bf6aaf85641b9d24399e921a
Matt Brennan <mattyb1000@gmail.com>
2016-07-05 15:09:24 +0100

    more postmodel.get tests

--- test/models/post.js
+++ test/models/post.js
@@ -1,36 +1,62 @@
 'use strict';
 
-const stubs = {redis: require('fakeredis')};
+const moduleStubs = {redis: require('fakeredis')};
 process.env.REDIS_URL = 'http://unused:1337';
 
 const sinon = require('sinon');
 const proxyquire = require('proxyquire');
 const expect = require('../../test-utils/expect');
 
-const redisClient = proxyquire('../../build/lib/redisClient', stubs);
+const redisClient = proxyquire('../../build/lib/redisClient', moduleStubs);
 const fbApi = require('../../build/lib/fbApi');
 const postModel = require('../../build/models/post');
 const database = require('../../build/lib/database');
 
 describe('Post model', () => {
 	describe('get', () => {
+		const stubs = [];
+
 		before(() => {
-			sinon.stub(fbApi, 'posts');
-			sinon.stub(database, 'getLastABCheck');
+			stubs.push.apply(stubs, [
+				sinon.stub(fbApi, 'posts'),
+				sinon.stub(database, 'getLastABCheck'),
+				sinon.stub(database, 'setLastABCheck'),
+			]);
+
+			fbApi.posts.returns([]);
+		});
+
+		beforeEach(() => {
+			stubs.forEach(stub => stub.reset());
 		});
 
 		after(async function() {
-			fbApi.posts.restore();
+			stubs.forEach(stub => stub.restore());
 			await redisClient.flushdbAsync();
 		});
 
 		it('should get posts since timestamp', async function() {
 			const since = 14e11;
-			fbApi.posts.returns([]);
 			database.getLastABCheck.returns(since);
 
 			await postModel.get();
 			expect(fbApi.posts).to.have.been.calledWithMatch({since});
+		});
+
+		it('shouldn\'t get posts if it\'s never been called before', async function() {
+			database.getLastABCheck.returns(0);
+
+			await postModel.get();
+			expect(fbApi.posts).not.to.have.been.called();
+		});
+
+		it('should set last ab run flag to current date', async function() {
+			const since = 14e11;
+			database.getLastABCheck.returns(since);
+
+			await postModel.get();
+			expect(database.setLastABCheck).to.have.been.called();
+			expect(database.setLastABCheck.lastCall.args[0]).to.be.above(since);
 		});
 	});
 });
