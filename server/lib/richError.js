aa8e17e1dd306e1e4c93e31f78e8ba3563773396
George Crawford <george.crawford@ft.com>
2016-07-15 16:03:18 +0100

    Un-scrub data where possible

--- server/lib/richError.js
+++ server/lib/richError.js
@@ -1,6 +1,28 @@
 'use strict';
 
 const util = require('util');
+const scrubbers = ['password', 'secret', 'passwd', 'authorization', 'api_key', 'apikey', 'access_token'];
+
+const unscrub = value => {
+	let ret;
+	switch(Object.prototype.toString.call(value)) {
+		case '[object Array]':
+			return value.map(unscrub);
+		case '[object Object]':
+			ret = {};
+			Object.keys(value).forEach(field => {
+				ret[field] = unscrub(value[field]);
+			});
+			return ret;
+		case '[object String]':
+			scrubbers.forEach(key => {
+				value = value.replace(new RegExp(`${key}((.{25})|(.*$))`, 'mg'), `[FILTERED]${key.split('').join(' ')}...[/FILTERED]`);
+			});
+			return value;
+		default:
+			return value;
+	}
+};
 
 function RichError(message, metadata = {}) {
 	this.message = message;
@@ -9,7 +31,9 @@ function RichError(message, metadata = {}) {
 	if(Error.captureStackTrace) Error.captureStackTrace(this, RichError);
 
 	// Copy all metadata fields to the Error object to be later sent to Sentry
-	Object.keys(metadata).forEach(field => (this[field] = metadata[field]));
+	Object.keys(metadata).forEach(field => {
+		this[field] = unscrub(metadata[field]);
+	});
 }
 
 util.inherits(RichError, Error);
