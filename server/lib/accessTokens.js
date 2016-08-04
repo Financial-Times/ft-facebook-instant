398522c7b38d1d8dbf501667893cba2c72933cd5
Matt Brennan <mattyb1000@gmail.com>
2016-08-04 11:48:15 +0100

    protect against undefined access tokens

--- server/lib/accessTokens.js
+++ server/lib/accessTokens.js
@@ -18,5 +18,7 @@ exports.get = () => redisClient.hvalsAsync('meta:access_tokens_hash')
 
 exports.remove = userId => redisClient.hdelAsync('meta:access_tokens_hash', userId);
 
-exports.add(process.env.FB_INITIAL_USER_ID, process.env.FB_PAGE_ACCESS_TOKEN)
-  .then(() => console.log(`${new Date()}:`, 'ACCESS TOKENS:', 'Added initial page token to Redis'));
+if(process.env.FB_INITIAL_USER_ID && process.env.FB_PAGE_ACCESS_TOKEN) {
+	exports.add(process.env.FB_INITIAL_USER_ID, process.env.FB_PAGE_ACCESS_TOKEN)
+		.then(() => console.log(`${new Date()}:`, 'ACCESS TOKENS:', 'Added initial page token to Redis'));
+}
